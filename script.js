e6da906498b5d7796c19e85412c1e3c351d1f32c
Joao Pedro Henderson Sarruf <48034139+joaosarruf@users.noreply.github.com>
2023-07-21 19:35:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
-let usuarioLogado = null; // Variável para armazenar o usuário logado
-
-function fazerLogin() {
-    const usuario = document.getElementById('username').value;
-    const senha = document.getElementById('password').value;
-
-    // Verificar as credenciais (neste exemplo, usuário: "admin", senha: "1234")
-    if (usuario === "admin" && senha === "1234") {
-        usuarioLogado = usuario; // Armazena o usuário logado
-        localStorage.setItem("usuarioLogado", usuario); // Salva o usuário no localStorage
-        mostrarPaginaPrincipal();
-    } else {
-        alert("Usuário ou senha inválidos. Tente novamente.");
-    }
-}
-
-function mostrarRegistro() {
-    document.querySelector('.login-form').style.display = 'none';
-    document.querySelector('.register-form').style.display = 'block';
-}
-
-function mostrarLogin() {
-    document.querySelector('.register-form').style.display = 'none';
-    document.querySelector('.login-form').style.display = 'block';
-}
-
-function criarConta() {
-    const novoUsuario = document.getElementById('new-username').value;
-    const novaSenha = document.getElementById('new-password').value;
-
-    // Verificar se os campos estão preenchidos (você pode adicionar mais validações aqui)
-    if (novoUsuario && novaSenha) {
-        usuarioLogado = novoUsuario; // Armazena o usuário logado
-        localStorage.setItem("usuarioLogado", novoUsuario); // Salva o usuário no localStorage
-        mostrarPaginaPrincipal();
-    } else {
-        alert("Preencha todos os campos para criar uma conta.");
-    }
-}
-
-function mostrarPaginaPrincipal() {
-    document.querySelector('.login-form').style.display = 'none';
-    document.querySelector('.register-form').style.display = 'none';
-    document.querySelector('.pagina-principal').style.display = 'block';
-    const usuarioSalvo = localStorage.getItem("usuarioLogado"); // Recupera o usuário do localStorage
-    if (usuarioSalvo) {
-        usuarioLogado = usuarioSalvo;
-        document.getElementById('username-display').innerText = usuarioLogado;
-    }
-}
-
 function registrarGasto() {
-    // Restante do código para registro de gastos e atualização dos valores (conforme o código original)
-    // ...
-}
+    const valorInput = parseFloat(document.getElementById('valor_input').value);
+    const tipoSelect = document.getElementById('tipo_select').value;
+    const categoriaSelect = document.getElementById('categoria_select');
+    const categoriaSelecionada = categoriaSelect.options[categoriaSelect.selectedIndex].value;
 
-// Inicializar a página
-window.onload = function() {
-    const usuarioSalvo = localStorage.getItem("usuarioLogado");
-    if (usuarioSalvo) {
-        usuarioLogado = usuarioSalvo;
-        mostrarPaginaPrincipal();
-    } else {
-        mostrarLogin();
+    if (isNaN(valorInput) || valorInput <= 0) {
+        alert('Digite um valor válido maior que zero.');
+        return;
     }
+
+    const saldoElement = document.getElementById('saldo');
+    const saldoAtual = parseFloat(saldoElement.innerText);
+    let novoSaldo;
+
+    if (tipoSelect === 'entrada') {
+        const categoriaElement = document.getElementById(categoriaSelecionada);
+        const valorCategoria = parseFloat(categoriaElement.innerText);
+        novoSaldo = saldoAtual + valorInput;
+        categoriaElement.innerText = (valorCategoria + valorInput).toFixed(2);
+    } else if (tipoSelect === 'retirada') {
+        const categoriaElement = document.getElementById(categoriaSelecionada);
+        const valorCategoria = parseFloat(categoriaElement.innerText);
+
+        if (valorCategoria >= valorInput) {
+            novoSaldo = saldoAtual - valorInput;
+            categoriaElement.innerText = (valorCategoria - valorInput).toFixed(2);
+        } else {
+            alert('Saldo insuficiente na categoria selecionada.');
+            return;
+        }
+    }
+
+    saldoElement.innerText = novoSaldo.toFixed(2);
+    document.getElementById('valor_input').value = '';
 }
